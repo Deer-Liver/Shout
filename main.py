4692ae57baf35b9a5fb01c5844445f13b9cb7419
Connor Glazer <deerliver.contact@gmail.com>
2025-12-12 13:28:12 +0000

    First test of  saving

---
+++ main.py
@@ -0,0 +1,8 @@
+# Source - https://stackoverflow.com/a
+# Posted by dmarth, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-12, License - CC BY-SA 4.0
+
+while True:
+    fd = open('test.txt',"w")
+    input = input("user input")
+    fd.write(input)
